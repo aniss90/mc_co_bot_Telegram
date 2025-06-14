66217a5fcf816af51e1bffb6e76330b703b35b16
aniss90 <anisluonadi99@gmail.com>
2025-06-14 02:47:25 +0100

    update

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("السلام عليكم! أنا بوتك شغال 🎉")
+    await update.message.reply_text("السلام عليكم! أنا البوت شغال 🎉")
 
 if __name__ == '__main__':
     app = ApplicationBuilder().token("7932950331:AAGkYxQOlV04JBjXuvDW3_8qjvaE8TTnXNc").build()
