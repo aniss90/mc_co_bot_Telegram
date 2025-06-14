80c20cddc1331dc73ccd391d321e7095cc366d1b
aniss90 <anisluonadi99@gmail.com>
2025-06-14 02:55:57 +0100

    update

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import os
+
+TOKEN = os.getenv("7932950331:AAGkYxQOlV04JBjXuvDW3_8qjvaE8TTnXNc")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("السلام عليكم! أنا البوت شغال 🎉")
+    await update.message.reply_text("السلام عليكم! ✅ البوت شغال.")
 
 if __name__ == '__main__':
-    app = ApplicationBuilder().token("7932950331:AAGkYxQOlV04JBjXuvDW3_8qjvaE8TTnXNc").build()
+    app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
